12aa48fd531242af5010061442906fc58a0a6234
ScoutWiki admin <support@scoutwiki.org>
2022-08-30 00:44:58 +0300

    Fix the default label

--- includes/WikibaseDictTag.php
+++ includes/WikibaseDictTag.php
@@ -20,7 +20,6 @@ class WikibaseDictTag {
 		// Get the current language for the header.
 		$contLang = MediaWikiServices::getInstance()->getContentLanguage();
 
-
 		// Get a list of all the languages for the term filtering.
 		$langUtils = MediaWikiServices::getInstance()->getLanguageNameUtils();
 		// FIXME: This doesn't work either.
@@ -37,15 +36,23 @@ class WikibaseDictTag {
 		
 		try {
 			$entity = $entityLookup->getEntity($itemId);
-			$output = '<table class="partiosanasto">
-			<tr><th colspan="2" class="partiosanasto_h1">Sana: ' . $labels[$contLang->getCode()] . ' <a href="https://dict.scoutwiki.org/">Partiosanastossa</a></th></tr>
-			<tr class="partiosanasto_h2"><th>Kieli</th><th>Sana</th></tr>';
+
+			$labelOutput = '';
+			$defaultLabel = NULL;
 			foreach ($entity->getLabels() as $label) {
+				if ($defaultLabel === NULL || $label->getLanguageCode() == $contLang) {
+					$defaultLabel = $label->getText();
+				}
 				// FIXME: Why doesn't this work?
 //				$langName = $langUtils->getLanguageName($label->getLanguageCode(), $contLang);
 				$langName = $langUtils->getLanguageName($label->getLanguageCode());
-				$output.= '<tr><th>' . $langName . '</th><td>' . $label->getText() . '</td></tr>';
+				$labelOutput .= '<tr><th>' . $langName . '</th><td>' . $label->getText() . '</td></tr>';
 			}
+
+			$output = '<table class="partiosanasto">
+			<tr><th colspan="2" class="partiosanasto_h1">Sana: ' . $defaultLabel . ' <a href="https://dict.scoutwiki.org/">Partiosanastossa</a></th></tr>
+			<tr class="partiosanasto_h2"><th>Kieli</th><th>Sana</th></tr>';
+			$output .= $labelOutput;
 			$output .= '</table>';
 		} catch (TermLookupException $exception) {
 			return null;
