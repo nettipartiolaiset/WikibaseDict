14ccd274c15c7cc16aac8c84c6dfee1c5b9d715e
Jyri-Petteri Paloposki <jyri-petteri.paloposki@iki.fi>
2022-08-29 23:31:59 +0300

    Initial working version

---
+++ includes/WikibaseDictTag.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace MediaWiki\Extension\WikibaseDict;
+
+use MediaWiki\MediaWikiServices;
+use Wikibase\Client\WikibaseClient;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Services\Lookup\TermLookupException;
+use ExtensionRegistry;
+
+class WikibaseDictTag {
+	
+	function render($input, $argv) {
+		$langs = ['en', 'fi', 'nl'];
+		
+		if (!ExtensionRegistry::getInstance()->isLoaded('WikibaseClient')) {
+		  return null;
+		}
+		
+		$contLang = MediaWikiServices::getInstance()->getContentLanguage();
+		$termLookup = WikibaseClient::getTermLookup();
+		try {
+		  $itemId = new ItemId( $input );
+		} catch ( InvalidArgumentException $exception ) {
+		  return null;
+		}
+		
+		try {
+		  $labels = $termLookup->getLabels($itemId, $langs);
+		  $output = '<table class="partiosanasto">
+		  <tr><th colspan="2" class="partiosanasto_h1">Sana: ' . $labels[$contLang->getCode()] . ' <a href="https://dict.scoutwiki.org/">Partiosanastossa</a></th></tr>
+		  <tr class="partiosanasto_h2"><th>Kieli</th><th>Sana</th></tr>';
+		  foreach ($labels as $lang => $label) {
+		    $output.= '<tr><th>' . $lang . '</th><td>' . $label . '</td></tr>';
+		  }
+		  $output .= '</table>';
+		} catch ( TermLookupException $exception ) {
+		  return null;
+		}
+		return $output;
+	}
+
+}
